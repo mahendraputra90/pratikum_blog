ab53fc48628953438eb6b0796e0f1068d9b2c61f
Mahendra Putra <80258331+mahendraputra90@users.noreply.github.com>
2021-05-17 21:44:37 +0800

    Add files via upload

---
+++ header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+ 
+<div class="header">
+ <h1>MAHENDRA BLOG</h1>
+ <p>Bersenang - senang dengan tenang</p>
+</div>
+<?php require "koneksi.php"; ?>
+<!-- Menampikan kolom sebelah kiri -->
+<div class="row">
+ <div class="leftcolumn">
---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+ 
+<div class="header">
+ <h1>Mahendra Blog</h1>
+ <p>Bersenang - Senang dengan Tenang</p>
+</div>
+ 
+<!-- Menampikan kolom sebelah kiri -->
+<div class="row">
+ <div class="leftcolumn">
+
+   <div class="card">
+     <a href="post.php"><h2>PENGALAMAN SAYA</h2></a>
+     <h5>Title description, Dec 7, 2017</h5>
+     <div class="fakeimg" style="height:200px;">Image</div>
+   </div>
+   <div class="card">
+     <a href="post.php"><h2>PEKERJAAN SAYA</h2></a>
+     <h5>Title description, Dec 7, 2017</h5>
+     <div class="fakeimg" style="height:200px;">Image</div>
+   </div>
+   <div class="card">
+     <a href="post.php"><h2>TITLE HEADING</h2></a>
+     <h5>Title description, Dec 7, 2017</h5>
+     <div class="fakeimg" style="height:200px;">Image</div>
+   </div>
+   <div class="card">
+     <a href="post.php"><h2>TITLE HEADING</h2></a>
+     <h5>Title description, Dec 7, 2017</h5>
+     <div class="fakeimg" style="height:200px;">Image</div>
+   </div>
+   <div class="card">
+     <a href="post.php"><h2>TITLE HEADING</h2></a>
+     <h5>Title description, Dec 7, 2017</h5>
+     <div class="fakeimg" style="height:200px;">Image</div>
+   </div>
+ </div>
+ 
+ 
+ <!-- Menampikan kolom sebelah kanan -->
+ <div class="rightcolumn">
+   <div class="card">
+     <h2>About Me</h2>
+     <div class="fakeimg" style="height:100px;">Image</div>
+     <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
+   </div>
+   <div class="card">
+     <h3>Popular Post</h3>
+     <a href="post.php"><div class="fakeimg"><p>Image</p></div></a>
+     <a href="post.php"><div class="fakeimg"><p>Image</p></div></a>
+     <a href="post.php"><div class="fakeimg"><p>Image</p></div></a>
+   </div>
+   <div class="card">
+     <h3>Follow Me on Instagram</h3>
+     <p>@mahendrakoyo</p>
+   </div>
+ </div>
+</div>
+ 
+<div class="footer">
+ <h2>Footer</h2>
+</div>
+
+ 
+</body>
+</html>
